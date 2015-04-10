1b56fbc09947ef79519c6ece42970273c4fe9b6d
sjbgit <sjb_strat@hotmail.com>
2015-04-10 16:24:36 -0500

    change cors

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+
+
+
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
+
 io.on('connection', function(socket){
     socket.on('event:new:image',function(data){
         socket.broadcast.emit('event:incoming:image',data);
