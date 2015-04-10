3a79906b5fe7cdec62d8dccdc7c16c19c2ef16cb
sjbgit <sjb_strat@hotmail.com>
2015-04-10 16:32:04 -0500

    try another cors

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 
+/*
+app.configure(function () {
+    router.use(express.static(path.resolve(__dirname, 'client')));
+    //router.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
+    router.use(express.logger('dev')); 						// log every request to the console
+    router.use(express.bodyParser()); 							// pull information from html in POST
+    router.use(express.methodOverride()); 						// simulate DELETE and PUT
+});
+*/
 
-
-app.all('*', function (req, res, next) {
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
