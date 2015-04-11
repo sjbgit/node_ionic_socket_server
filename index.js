a6026966e2b7e263328c278554c749e80a25c045
sjbgit <sjb_strat@hotmail.com>
2015-04-10 19:15:27 -0500

    index.js is handling posts

--- index.js
+++ index.js
@@ -1,22 +1,127 @@
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+/**
+ * Created by sbunke on 4/10/2015.
+ */
+//
+// # SimpleServer
+//
+// A simple chat server using Socket.IO, Express, and Async.
+//
+var http = require('http');
+var path = require('path');
+
+var async = require('async');
+var socketio = require('socket.io');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+//
+// ## SimpleServer `SimpleServer(obj)`
+//
+// Creates a new instance of SimpleServer with the following options:
+//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
+//
+var router = express();
+var server = http.createServer(router);
+var io = socketio.listen(server);
+
+//http://stackoverflow.com/questions/25550819/error-most-middleware-like-bodyparser-is-no-longer-bundled-with-express
+// parse application/json
+router.use(bodyParser.json());
+
+// parse application/x-www-form-urlencoded
+router.use(bodyParser.urlencoded({ extended: true }));
+
+router.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
+router.get('/api/notify', function (req, res) {
+    res.json({message: 'test message from server'}); // return all todos in JSON format
+});
+
+router.post('/api/perftest', function (req, res) {
+    res.json({message: 'Perf test - test message from server'}); // return all todos in JSON format
+});
+
+
+router.post('/api/message', function(req, res) {
+
+    console.log(req.body);
+
+    var body = req.body;
+
+    var message = body.message;
+
+    broadcast('event:incoming:message', body);
+
+    res.json({message: message + ' from server'});
+
+
+});
+
+var sockets = [];
+
+
+function broadcast(event, data) {
+    sockets.forEach(function (socket) {
+        socket.emit(event, data);
+    });
+}
+
+server.listen(process.env.PORT || 8000, process.env.IP || "0.0.0.0", function () {
+    var addr = server.address();
+    console.log("Chat server listening at", addr.address + ":" + addr.port);
+});
+
+io.on('connection', function(socket){
+    sockets.push(socket);
+    socket.on('event:new:image',function(data){
+        socket.broadcast.emit('event:incoming:image',data);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*
-app.configure(function () {
-    router.use(express.static(path.resolve(__dirname, 'client')));
-    //router.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
-    router.use(express.logger('dev')); 						// log every request to the console
-    router.use(express.bodyParser()); 							// pull information from html in POST
-    router.use(express.methodOverride()); 						// simulate DELETE and PUT
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var router = express.Router();
+
+console.log(router);
+
+
+router.route('/movies').get(function(req, res) {
+    res.json({message: 'Perf test - test message from server'}); // return all todos in JSON format
 });
-*/
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
+});
+
+
+app.post('/api/perftest', function (req, res) {
+    res.json({message: 'Perf test - test message from server'}); // return all todos in JSON format
 });
 
 
@@ -28,6 +133,8 @@ io.on('connection', function(socket){
 
 server.listen(8000,function(){
     console.log('Socket.io Running');
-});/**
+});
+
+/**
  * Created by sbunke on 4/9/2015.
  */
